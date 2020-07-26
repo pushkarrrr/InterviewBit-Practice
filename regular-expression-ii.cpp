8d7968653c253cbd4abb7fca3f5134b9de73e9e4
Pushkar Jaiswal <44117060+pushkarrrr@users.noreply.github.com>
2020-07-27 02:21:44 +0530

    Create regular-expression-ii.cpp

---
+++ regular-expression-ii.cpp
@@ -0,0 +1,44 @@
+int Solution::isMatch(const string A, const string B) {
+    int n=A.size();
+    int m=B.size();
+    bool dp[n+1][m+1];
+    dp[0][0]=true;
+    for(int i=1;i<=n;i++)
+    {
+        dp[i][0]=false;
+    }
+    for(int j=1;j<=m;j++)
+    {
+        if(B[j-1]=='*')
+        {
+            dp[0][j]=dp[0][j-2];
+        }
+        else
+        {
+            dp[0][j]=false;
+        }
+    }
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=m;j++)
+        {
+            if((A[i-1]==B[j-1])||B[j-1]=='.')
+            {
+                dp[i][j]=dp[i-1][j-1];
+            }
+            else if(B[j-1]=='*')
+            {
+                dp[i][j]=dp[i][j-2];
+                if(B[j-2]=='.'||B[j-2]==A[i-1]) //-1 indexing
+                {
+                    dp[i][j]=dp[i][j]||dp[i-1][j];
+                }
+            }
+            else
+            {
+                dp[i][j]=false;
+            }
+        }
+    }
+    return dp[n][m];
+}
